16ea9667f1b675afcef4b03e5b9f53aabf0b6089
ssbtvnx <sabinaashimbekova@gmail.com>
2024-03-24 01:53:53 +0600

    lab7

---
+++ lab7/informatics/loops/for/k.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+sum = 0
+for _ in range(n):
+    num = int(input())
+    sum += num
+
+print(sum)
---
+++ lab7/informatics/loops/for/l.py
@@ -0,0 +1,7 @@
+binary = input()
+decimal = 0
+
+for i in range(len(binary)):
+    decimal += int(binary[i]) * (2 ** (len(binary) - i - 1))
+
+print(decimal)
